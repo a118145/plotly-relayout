200bc569878bbcf23420a867ea6ddb80ed6e5f1c
a118145 <arne@hansen-doerr.de>
2026-02-03 19:14:41 +0100

    MWE

--- custom_components/reflex_plotly_relayout/plotly_relayout.py
+++ custom_components/reflex_plotly_relayout/plotly_relayout.py
@@ -3,7 +3,15 @@
 
 # For wrapping react guide, visit https://reflex.dev/docs/wrapping-react/overview/
 
+from typing import Any, List, Dict, TypedDict
+
 import reflex as rx
+from reflex.components.plotly.plotly import Plotly
+from reflex.vars.base import Var
+# from reflex.event import EventHandler
+# from reflex.vars.function import FunctionVar
+from reflex.vars.object import ObjectVar
+# from dataclasses import dataclass
 
 # Some libraries you want to wrap may require dynamic imports.
 # This is because they they may not be compatible with Server-Side Rendering (SSR).
@@ -13,15 +21,46 @@ import reflex as rx
 # class PlotlyRelayout(NoSSRComponent):
 #     pass
 
+class _ClickEvent(TypedDict):
+    """Defines the shape of the click event passed to onClick by react-plotly.js."""
+    # event: Any
+    points: List[Dict]
 
-class PlotlyRelayout(rx.Component):
+def _on_click_signature(event: ObjectVar[_ClickEvent]) -> List[Any]:
+    """Maps the onClick args from javascript to the event handled by the Reflex backend in python."""
+    # return [event.point_info]
+    return [
+        # event.points
+        rx.Var(
+            _js_expr = f"myExtractPoints({event.points})",
+        ),
+    ]
+
+class _RelayoutEvent(TypedDict):
+    """Defines the shape of the relayout event passed to onRelayout by react-plotly.js."""
+    # event: Any
+    # points: List[Dict]
+
+def _on_relayout_signature(event: ObjectVar[_RelayoutEvent]) -> List[Any]:
+    """Maps the onRelayout args from javascript to the event handled by the Reflex backend in python."""
+    # return [event.point_info]
+    return [
+        # event.points
+        'This is a test',
+        # rx.Var(
+        #     _js_expr = f"console.log({event})",
+        # ),
+    ]
+
+
+class PlotlyRelayout(Plotly):
     """PlotlyRelayout component."""
 
     # The React library to wrap.
-    library = "Fill-Me"
+    # library = "Fill-Me"
 
     # The React component tag.
-    tag = "Fill-Me"
+    # tag = "Fill-Me"
 
     # If the tag is the default export from the module, you must set is_default = True.
     # This is normally used when components don't have curly braces around them when importing.
@@ -49,10 +88,34 @@ class PlotlyRelayout(rx.Component):
     # The function defined for the `on_change` trigger maps event for the javascript
     # trigger to what will be passed to the backend event handler function.
     # on_change: rx.EventHandler[lambda e: [e]]
+    on_click: rx.EventHandler[_on_click_signature]
+    # on_click: rx.EventHandler[lambda e: [print(e)]]
+    on_relayout: rx.EventHandler[_on_relayout_signature]
 
     # To add custom code to your component
-    # def _get_custom_code(self) -> str:
-    #     return "const customCode = 'customCode';"
+    def _get_custom_code(self) -> str:
+        return """
+// extract only relevant data from the points to minimize payload size and workaround circular references
+const myExtractPoints = (points) => {
+    return points.map(point => {
+        return {
+            x: point.x,
+            y: point.y,
+            curveNumber: point.curveNumber,
+            pointNumber: point.pointNumber,
+            pointIndex: point.pointIndex,
+            'marker.color': point['marker.color'],
+            'marker.size': point['marker.size'],
+            bbox: {
+                x0: point.bbox.x0,
+                x1: point.bbox.x1,
+                y0: point.bbox.y0,
+                y1: point.bbox.y1,
+            }
+        }
+    })
+}
+"""
 
 
 plotly_relayout = PlotlyRelayout.create
--- plotly_relayout_demo/plotly_relayout_demo/plotly_relayout_demo.py
+++ plotly_relayout_demo/plotly_relayout_demo/plotly_relayout_demo.py
@@ -4,27 +4,103 @@
 from rxconfig import config
 
 import reflex as rx
+import plotly.graph_objects as go
+import numpy as np
 
-# from reflex_plotly_relayout import plotly_relayout
-
-filename = f"{config.app_name}/{config.app_name}.py"
+from reflex_plotly_relayout import plotly_relayout
 
 
 class State(rx.State):
     """The app state."""
-    pass
+    fig: go.Figure = go.Figure()
+
+    RAND_SIZE: int = 10
+
+    @rx.var
+    def plotly_fig(self) -> go.Figure:
+        return self.fig
+
+    @rx.event
+    def load_figures(self):
+        x = np.arange(self.RAND_SIZE)
+        y = np.random.rand(self.RAND_SIZE) 
+
+        self.fig = go.Figure(
+            [go.Scattergl(
+                x = x, 
+                y = y, 
+                mode = 'lines',
+            ),
+            ],
+        )
+        self.fig.update_layout(
+            autosize=True,
+            margin=dict(l=0, r=0, t=0, b=0),
+            font=dict(size=25, family='Inter, ui-sans-serif, system-ui'),
+        )
+        # self.fig = fig
+
+    @rx.event
+    def plotly_click_event(self, point: list):
+        print("Click event")
+        print(point)
+
+    @rx.event
+    def plotly_relayout_event(self):
+        print("Relayout event")
+
+    @rx.event
+    def plotly_relayout_click_event(self, point: list):
+        print("Custom click event")
+        print(point)
+
+    @rx.event
+    def plotly_relayout_relayout_event(self, obj):
+        print("Custom relayout event")
+        print(obj)
+
+
+
 
 def index() -> rx.Component:
     return rx.center(
         # rx.theme_panel(),
         rx.vstack(
             rx.heading("Welcome to the Plotly relayout demo!", size="9"),
-            # rx.text(
-            #     "Test your custom component by editing ",
-            #     rx.code(filename),
-            #     font_size="2em",
-            # ),
-            # plotly_relayout(),
+            rx.hstack(
+                rx.card(
+                    rx.vstack(
+                        rx.code(
+                            "Default rx.plotly",
+                            size = '5',
+                        ),
+                        rx.plotly(
+                            data = State.plotly_fig,
+                            on_click = State.plotly_click_event,
+                            on_relayout = State.plotly_relayout_event,
+                        ),
+                        align = 'center',
+                    ),
+                    size = '4',
+                ),
+                rx.card(
+                    rx.vstack(
+                        rx.code(
+                            "Custom plotly_relayout",
+                            size = '5',
+                        ),
+                        # rx.plotly(
+                        plotly_relayout(
+                            data = State.plotly_fig,
+                            on_click = State.plotly_relayout_click_event,
+                            # on_click = State.plotly_click_event,
+                            on_relayout = State.plotly_relayout_relayout_event,
+                        ),
+                        align = 'center',
+                    ),
+                    size = '4',
+                ),
+            ),
             align="center",
             spacing="7",
         ),
@@ -34,4 +110,7 @@ def index() -> rx.Component:
 
 # Add state and page to the app.
 app = rx.App()
-app.add_page(index)
+app.add_page(
+    index,
+    on_load = State.load_figures,
+    )
